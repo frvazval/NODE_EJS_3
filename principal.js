0043def63f9e77a011eae54a76162d00eed3f42a
Fvazquez <foapgenerico@365.stucom.com>
2025-06-27 17:46:40 +0200

    stucom

--- principal.js
+++ principal.js
@@ -1,3 +1,9 @@
  import {calculadora} from "./funciones.js";
+//  const fs = require('node:fs'); esto no funcionaria, porque en package.json hemos puesto "type": "module"
+import fs from "node:fs";
+fs.writeFileSync("test.txt", "Hoy es viernes", "utf-8");
+
+import express from 'express';
 
  console.log(calculadora(5,10,"sumar"));
+ console.log(calculadora(5,10,"restar"));
