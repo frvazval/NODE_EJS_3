406a2af08dda5c3f1d02f76b9eb3ce8a4d041dec
Fvazquez <foapgenerico@365.stucom.com>
2025-06-27 17:48:12 +0200

    stucom

--- principal.js
+++ principal.js
@@ -2,7 +2,7 @@
 //  const fs = require('node:fs'); esto no funcionaria, porque en package.json hemos puesto "type": "module"
 import fs from "node:fs";
 fs.writeFileSync("test.txt", "Hoy es viernes", "utf-8");
-
+// const express = require("express");
 import express from 'express';
 
  console.log(calculadora(5,10,"sumar"));
